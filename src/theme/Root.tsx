b80b12d1209e138ed3db04080651bbbfca804ebb
DevomB <Devom.hb@yahoo.com>
2026-06-05 00:34:16 -0700

    Remove @vercel/speed-insights dependency and related component from RootWrapper

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -1,13 +1,7 @@
 import React from 'react';
 import Root from '@theme-original/Root';
 import type {Props} from '@theme/Root';
-import {SpeedInsights} from '@vercel/speed-insights/react';
 
 export default function RootWrapper(props: Props): React.JSX.Element {
-  return (
-    <>
-      <Root {...props} />
-      <SpeedInsights />
-    </>
-  );
+  return <Root {...props} />;
 }
